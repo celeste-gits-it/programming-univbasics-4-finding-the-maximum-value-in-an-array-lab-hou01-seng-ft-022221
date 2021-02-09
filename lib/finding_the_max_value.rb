a2c94657a8949423d847812434f7b4cfab421fbc
Celeste Carter <celesteanell@gmail.com>
2021-02-09 21:16:11 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,8 +2,8 @@
    count = 0
    
   max_value = -1
-  while count <= array.length do
-    if max_value <= array[count]
+  while count < array.length do
+    if max_value < array[count]
       max_value = array[count]
     end
     count += 1
